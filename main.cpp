ffae672b63dd6cac13b77f9ecae0be05e0bcf89e
polchasa <polchasa@live.ru>
2023-09-10 14:39:31 +0300

    README.md updated

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <clocale>
 #include "game.cpp"
 
 using namespace std;
 
 int main() { 
+    setlocale(LC_ALL,"Russian");
     Game game;
     game.startGame();
     return 0;
